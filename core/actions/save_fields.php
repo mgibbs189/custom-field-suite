72d9068e22fd9cd9acb03b79e705fdd49dbc2c16
Matt <logikal16@gmail.com>
2012-09-16 23:39:12 -0400

    Remove values for deleted fields

--- core/actions/save_fields.php
+++ core/actions/save_fields.php
@@ -12,27 +12,28 @@ $cfs_extras = isset($_POST['cfs']['extras']) ? $_POST['cfs']['extras'] : array()
 
 $weight = 0;
 $prev_fields = array();
+$current_field_ids = array();
 $table_name = $wpdb->prefix . 'cfs_fields';
 
-// get existing fields (check for renamed or deleted fields)
-$results = $wpdb->query("SELECT id, name FROM $table_name WHERE post_id = '$post_id'");
+// Get existing fields (check for renamed or deleted fields)
+$results = $wpdb->get_results("SELECT id, name FROM $table_name WHERE post_id = '$post_id'");
 foreach ($results as $result)
 {
     $prev_fields[$result->id] = $result->name;
 }
 
-// remove all existing fields
+// Remove all existing fields
 $wpdb->query("DELETE FROM $table_name WHERE post_id = '$post_id'");
 
 foreach ($field_data as $key => $field)
 {
-    // clean the field
+    // Sanitize the field
     $field = stripslashes_deep($field);
 
-    // allow for field customizations
+    // Allow for field customizations
     $field = $this->fields[$field['type']]->pre_save_field($field);
 
-    // save empty string for fields without options
+    // Save empty string for fields without options
     $field['options'] = !empty($field['options']) ? serialize($field['options']) : '';
 
     $data = array(
@@ -46,10 +47,13 @@ foreach ($field_data as $key => $field)
         'options' => $field['options'],
     );
 
-    // use an existing ID if available
+    // Use an existing ID if available
     if (0 < (int) $field['id'])
     {
         $data['id'] = (int) $field['id'];
+
+        // We use this variable to check for deleted fields
+        $current_field_ids[] = $data['id'];
 
         // Rename the postmeta key if necessary
         if ($field['name'] != $prev_fields[$data['id']])
@@ -66,10 +70,24 @@ foreach ($field_data as $key => $field)
         }
     }
 
-    // insert the field
+    // Insert the field
     $wpdb->insert($table_name, $data);
 
     $weight++;
+}
+
+// Remove values for deleted fields
+$deleted_field_ids = array_diff(array_keys($prev_fields), $current_field_ids);
+
+if (0 < count($deleted_field_ids))
+{
+    $deleted_field_ids = implode(',', $deleted_field_ids);
+    $wpdb->query("
+        DELETE v, m
+        FROM {$wpdb->prefix}cfs_values v
+        INNER JOIN {$wpdb->postmeta} m ON m.meta_id = v.meta_id
+        WHERE v.field_id IN ($deleted_field_ids)"
+    );
 }
 
 /*---------------------------------------------------------------------------------------------
