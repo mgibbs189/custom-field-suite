7371d68bad08fc62e1c9ea5d1941e236f3242fba
Matt Gibbs <mgibbs189@gmail.com>
2013-04-16 10:40:13 -0400

    Clarified license (#113)

--- cfs.php
+++ cfs.php
@@ -6,7 +6,19 @@ Description: Visually add custom fields to your WordPress edit pages.
 Version: 1.8.9
 Author: Matt Gibbs
 Author URI: http://uproot.us/
-License: GPL2
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses/>.
 */
 
 $cfs = new cfs();
