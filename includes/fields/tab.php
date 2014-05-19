0c4d61cf4c382d2c96a2014fc7091ac8c7bf5458
mgibbs189 <mgibbs189@gmail.com>
2014-05-19 15:00:23 -0400

    Added commenting

--- includes/fields/tab.php
+++ includes/fields/tab.php
@@ -6,20 +6,22 @@ class cfs_tab extends cfs_field
     function __construct( $parent ) {
         $this->name = 'tab';
         $this->label = __( 'Tab', 'cfs' );
-        $this->parent = $parent;
     }
 
 
+    // Prevent tabs from inheriting the parent field HTML
     function html( $field ) {
 
     }
 
 
+    // Prevent tabs from inheriting the parent options HTML
     function options_html( $key, $field ) {
 
     }
 
 
+    // Tab handling javascript
     function input_head( $field = null ) {
     ?>
         <script>
