de6f9dd1b74c663d58affa57588e91f9c6249331
mgibbs189 <mgibbs189@gmail.com>
2015-06-03 12:19:39 -0400

    Untranslated strings (#270)

--- includes/fields/relationship.php
+++ includes/fields/relationship.php
@@ -60,7 +60,7 @@ class cfs_relationship extends cfs_field
         }
     ?>
         <div class="filter_posts">
-            <input type="text" class="cfs_filter_input" autocomplete="off" placeholder="Search posts" />
+            <input type="text" class="cfs_filter_input" autocomplete="off" placeholder="<?php _e( 'Search posts', 'cfs' ); ?>" />
         </div>
 
         <div class="available_posts post_list">
