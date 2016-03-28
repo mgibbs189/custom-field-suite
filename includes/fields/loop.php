6da3723f79b9266752b1b58a2d3a87ef04e3189c
Chris Van Patten <chris@vanpattenmedia.com>
2016-03-28 15:40:25 -0400

    use javascript hrefs to add loop move/delete buttons to the tabindex

--- includes/fields/loop.php
+++ includes/fields/loop.php
@@ -75,7 +75,7 @@ class cfs_loop extends cfs_field
                 ?>
             </td>
         </tr>
-        
+
         <tr class="field_option field_option_<?php echo $this->name; ?>">
             <td class="label">
                 <label><?php _e( 'Limits', 'cfs' ); ?></label>
@@ -85,7 +85,7 @@ class cfs_loop extends cfs_field
                 <input type="text" name="cfs[fields][<?php echo $key; ?>][options][limit_max]" value="<?php echo $this->get_option( $field, 'limit_max' ); ?>" placeholder="max" style="width:60px" />
             </td>
         </tr>
-        
+
     <?php
     }
 
@@ -117,8 +117,8 @@ class cfs_loop extends cfs_field
     ?>
         <div class="loop_wrapper">
             <div class="cfs_loop_head open">
-                <a class="cfs_delete_field"></a>
-                <a class="cfs_toggle_field"></a>
+                <a class="cfs_delete_field" href="javascript:;"></a>
+                <a class="cfs_toggle_field" href="javascript:;"></a>
                 <span class="label"><?php echo esc_attr( $row_label ); ?></span>
             </div>
             <div class="cfs_loop_body open">
@@ -204,8 +204,8 @@ class cfs_loop extends cfs_field
     ?>
         <div class="loop_wrapper">
             <div class="cfs_loop_head<?php echo $css_class; ?>">
-                <a class="cfs_delete_field"></a>
-                <a class="cfs_toggle_field"></a>
+                <a class="cfs_delete_field" href="javascript:;"></a>
+                <a class="cfs_toggle_field" href="javascript:;"></a>
                 <span class="label"><?php echo esc_attr( $this->dynamic_label( $row_label, $results, $values[ $i ] ) ); ?>&nbsp;</span>
             </div>
             <div class="cfs_loop_body<?php echo $css_class; ?>">
