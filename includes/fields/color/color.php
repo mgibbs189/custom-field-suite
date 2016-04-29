894dab9934467050c8ce8122c3bbfa581ec5a871
mgibbs189 <mgibbs189@gmail.com>
2016-04-29 15:42:01 -0400

    Default value for color field (#293)

--- includes/fields/color/color.php
+++ includes/fields/color/color.php
@@ -9,6 +9,26 @@ class cfs_color extends cfs_field
     }
 
 
+    function options_html( $key, $field ) {
+    ?>
+        <tr class="field_option field_option_<?php echo $this->name; ?>">
+            <td class="label">
+                <label><?php _e( 'Default Value', 'cfs' ); ?></label>
+            </td>
+            <td>
+                <?php
+                    CFS()->create_field( array(
+                        'type' => 'text',
+                        'input_name' => "cfs[fields][$key][options][default_value]",
+                        'value' => $this->get_option( $field, 'default_value' ),
+                    ));
+                ?>
+            </td>
+        </tr>
+    <?php
+    }
+
+
     function input_head( $field = null ) {
         wp_register_script( 'miniColors', CFS_URL . '/includes/fields/color/jquery.miniColors.min.js' );
         wp_enqueue_script( 'miniColors' );
