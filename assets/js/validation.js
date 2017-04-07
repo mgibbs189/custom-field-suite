bc5b83f436acbf2ca796949194d857958e7fc85c
Chris Van Patten <hello@chrisvanpatten.com>
2017-04-07 17:09:47 -0400

    [WIP / RFC] Improve inline validation error handling (#363)
    * Make sure we open up loop rows if there are errors in them
    
    * use a global admin notice to indicate a validation error
    
    * WordPressify the inline error style
    
    * add @since 2.6!

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -123,16 +123,23 @@
                                     $this.append('<div class="error"></div>');
                                 }
 
+                                // if the error is inside a loop field, open it up
+                                if ($this.parents('.cfs_loop_body').length > 0) {
+                                    $loop = $this.parents('.cfs_loop_body');
+                                    $loop.addClass('open');
+                                    $loop.siblings('.cfs_loop_head').addClass('open');
+                                }
+
                                 // error can be either a string or function
                                 var error_msg = CFS.validators[validator]['error'];
                                 if ('function' == typeof error_msg) {
                                     error_msg = error_msg($this);
                                 }
+
                                 $this.find('.error').html(error_msg);
                                 $this.find('.error').show();
-                                $('html, body').animate({
-                                    scrollTop: $this.find('.error').offset().top
-                                }, 500);
+
+                                $('#cfs-validation-admin-notice').show();
                             }
                         }
                     });
