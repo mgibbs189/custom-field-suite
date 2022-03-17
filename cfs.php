e8f1e504ffd4c822eca75277ed23a23ad933fc28
Matt Gibbs <mgibbs189@fastmail.com>
2022-03-17 12:43:55 -0400

    Removed broken links

--- cfs.php
+++ cfs.php
@@ -1,9 +1,8 @@
 <?php
 /*
 Plugin Name: Custom Field Suite
-Plugin URI: http://customfieldsuite.com/
 Description: Visually add custom fields to your WordPress edit pages.
-Version: 2.6.1
+Version: 2.6.2
 Author: Matt Gibbs
 Text Domain: cfs
 Domain Path: /languages/
@@ -22,7 +21,7 @@ class Custom_Field_Suite
     function __construct() {
 
         // setup variables
-        define( 'CFS_VERSION', '2.6.1' );
+        define( 'CFS_VERSION', '2.6.2' );
         define( 'CFS_DIR', dirname( __FILE__ ) );
         define( 'CFS_URL', plugins_url( '', __FILE__ ) );
 
